ebc6dc707304027c92b6a16c0584d9a120c95b16
ioelgalantedv <ioel.galante@davinci.edu.ar>
2024-07-01 18:13:17 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,86 @@
+document.getElementById("boton").addEventListener("click", function(){
+    const data = document.getElementById("recital").value;
+    if(data){
+        //agregar al html y localStorage
+        AgregarRecital(data)
+        elimguardarRecitalLocal(data)
+        document.getElementById("ingresar").classList.add("hidden")
+        document.getElementById("inicio").classList.remove("hidden")
+
+        document.getElementById("data").value = "";
+    }else{
+        alert("Por favor ingresar algo.")
+    }
+})
+
+function AgregarRecital(recital){
+    const tarjeta = document.createElement("div")
+    tarjeta.classList.add("tarjeta")
+
+    const parrafo = document.createElement("p");
+    parrafo.textContent = recital;
+    tarjeta.appendChild(parrafo)
+
+    const acciones = document.createElement("div")
+    acciones.classList.add("acciones")
+
+    const eliminar = document.createElement("a")
+    eliminar.href = "#misRecitales"
+    eliminar.textContent = "Eliminar"
+    eliminar.classList.add("eliminar")
+    eliminar.addEventListener("click", function(){
+        tarjeta.remove();
+        eliminarRecitales(recital)
+    })
+
+    const modificar = document.createElement("a")
+    modificar.href = "#misRecitales"
+    modificar.textContent = "Modificar"
+    modificar.classList.add("modificar")
+    modificar.addEventListener("click", function(){
+        const nuevorecital = prompt("modificar recital: ", parrafo.textContent)
+        if(nuevorecital){
+            modificarRecitales(recital, nuevorecital)
+            parrafo.textContent = nuevorecital;
+        }
+    })
+
+    acciones.appendChild(eliminar)
+    acciones.appendChild(modificar)
+    tarjeta.appendChild(acciones)
+
+    document.getElementById("contenedor").appendChild(tarjeta)
+}
+
+function elimguardarRecitalLocal(recital){
+    let recitales = JSON.parse(localStorage.getItem("recitales")) || [];
+
+    recitales.push(recital);
+
+    localStorage.setItem("recitales", JSON.stringify(recitales))
+}
+
+function cargarRecitales(){
+    let recitales = JSON.parse(localStorage.getItem("recitales")) || [];
+    recitales.forEach(recital => {
+        AgregarRecital(recital)
+    });
+}
+
+function eliminarRecitales(recital){
+    let recitales = JSON.parse(localStorage.getItem("recitales")) || [];
+    recitales = recitales.filter(d => d != recital)
+    localStorage.setItem("recitales", JSON.stringify(recitales))
+}
+
+function modificarRecitales(recitalViejo, recitalNuevo){
+    let recitales = JSON.parse(localStorage.getItem("recitales")) || [];
+    const index = recitales.indexOf(recitalViejo)
+    if(index !== -1){
+        recitales[index] = recitalNuevo;
+        localStorage.setItem("recitales", JSON.stringify(recitales))
+    }
+}
+
+cargarRecitales()
+
